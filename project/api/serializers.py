f92d9d0eba9feafd8f8e863ec5199bd0e2550a13
Darwin Wu <darwin@berkeley.edu>
2018-10-18 15:12:21 -0700

    Pass black

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -11,62 +11,60 @@ class PoliticalEntitySerializer(serializers.ModelSerializer):
     """
     Serializes the PoliticalEntity model
     """
+
     class Meta:
         model = PoliticalEntity
-        exclude = ('polymorphic_ctype',)
+        exclude = ("polymorphic_ctype",)
 
 
 class TerritorySerializer(serializers.ModelSerializer):
     """
     Serializes the Territory model as GeoJSON compatible data
     """
-    entity = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='url_id'
-    )
+
+    entity = serializers.SlugRelatedField(read_only=True, slug_field="url_id")
 
     def to_internal_value(self, data):
         ret = {}
 
         # Update ret to include passed in data
         for field, val in data.items():
-            if field == 'entity':
-                ret['entity'] = PoliticalEntity.objects.get(pk=val)
-            if field != 'geo' and field != 'entity':
+            if field == "entity":
+                ret["entity"] = PoliticalEntity.objects.get(pk=val)
+            if field != "geo" and field != "entity":
                 ret[field] = val
 
         # Convert geo field to MultiPolygon if it is a FeatureCollection
-        geojson = loads(data['geo'])
-        if geojson['type'] == 'FeatureCollection':
-            features = geojson['features']
-            features_union = GEOSGeometry(dumps(features[0]['geometry']))
+        geojson = loads(data["geo"])
+        if geojson["type"] == "FeatureCollection":
+            features = geojson["features"]
+            features_union = GEOSGeometry(dumps(features[0]["geometry"]))
             features = features[1:]
 
             for feature in features:
-                if feature['geometry']['type'] == 'Polygon':
+                if feature["geometry"]["type"] == "Polygon":
                     features_union = features_union.union(
-                        GEOSGeometry(dumps(feature['geometry'])))
+                        GEOSGeometry(dumps(feature["geometry"]))
+                    )
 
-            ret['geo'] = features_union
+            ret["geo"] = features_union
         else:
-            ret['geo'] = data['geo']
+            ret["geo"] = data["geo"]
 
         return ret
 
     class Meta:
         model = Territory
-        fields = '__all__'
+        fields = "__all__"
 
 
 class DiplomaticRelationSerializer(serializers.ModelSerializer):
     """
     Serializes the DiplomaticRelation model
     """
-    entity = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='url_id'
-    )
+
+    entity = serializers.SlugRelatedField(read_only=True, slug_field="url_id")
 
     class Meta:
         model = DiplomaticRelation
-        fields = '__all__'
+        fields = "__all__"
